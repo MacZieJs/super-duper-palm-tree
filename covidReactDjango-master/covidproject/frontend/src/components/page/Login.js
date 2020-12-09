5d7672a0976fceaabf1404943d0ef67c9b6d8ad9
Tarun Menon <tmenon@wisc.edu>
2020-12-08 18:00:18 -0600

    changed login to react-bootstrap

--- covidReactDjango-master/covidproject/frontend/src/components/page/Login.js
+++ covidReactDjango-master/covidproject/frontend/src/components/page/Login.js
@@ -3,6 +3,7 @@ import AuthServices from '../../services/AuthServices'
 import LocalStorageServices from "../../services/LocalStorageServices";
 import AppContext from "../contexts/AppContext";
 import { useHistory } from "react-router-dom";
+import {Form, Button, Image} from 'react-bootstrap';
 
 const Login = (props) => {
     const [username, setUsername] = useState("")
@@ -27,7 +28,7 @@ const Login = (props) => {
     const handleFormSubmit = () => {
         loginReq()
     }
-    return (
+    /*return (
         <div>
             <h3>Login Page</h3>
             <form onSubmit={(e) => handleFormSubmit(e)}>
@@ -52,6 +53,22 @@ const Login = (props) => {
                 <div>{errMsg || null}</div>
             </form>
         </div>
-    )
+    )*/
+    return(
+        <div className="text-center">
+            <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtKy-Nz1UREsRH7FGafRb42-bZcjp-Q4imdw&usqp=CAU" />
+            <Form>
+                <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control size="sm" type="username" placeholder="Enter Username" onChange={(e) => setUsername(e.target.value)}/>
+                </Form.Group>
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)}/>
+                </Form.Group>
+            </Form>
+            <Button onClick={handleFormSubmit()}>Login</Button>
+        </div>
+    );
 }
 export default Login
